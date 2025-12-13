1b6027a2d224efa945626b644fdc190c92e3a339
Victor Pereira <victorpereirafernandes20070101@gmail.com>
2025-12-12 22:15:30 -0300

    foii

--- megadata-users/megadata-users/src/main/java/com/victor/megadata_users/config/CorsConfig.java
+++ megadata-users/megadata-users/src/main/java/com/victor/megadata_users/config/CorsConfig.java
@@ -1,23 +1,18 @@
 package com.victor.megadata_users.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                            "https://cadastro-de-usuarios-java-ru4e.vercel.app"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+        registry.addMapping("/**")
+                .allowedOrigins("https://cadastro-de-usuarios-java-ru4e.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
